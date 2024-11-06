e7ea1f01c322ea867c6da8c968f26cb5a7733036
medazizturki <mohamedazizturkiii@gmail.com>
2024-11-06 19:48:54 +0100

    last push devops5

--- src/main/java/tn/esprit/tpfoyer/control/BlocRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/BlocRestController.java
@@ -23,23 +23,19 @@ public class BlocRestController {
     @Operation(description = "WS de récuperation de tous les Blocs ")
     public List<Bloc> getBlocs() {
         return blocService.retrieveAllBlocs();
-        //return listBlocs;
     }
 
-
-    // http://localhost:8089/tpfoyer/bloc/retrieve-bloc/8
+    /////////////////////
     @GetMapping("/retrieve-bloc/{bloc-id}")
     public Bloc retrieveBloc(@PathVariable("bloc-id") Long bId) {
-        Bloc bloc = blocService.retrieveBloc(bId);
-        return bloc;
+        return blocService.retrieveBloc(bId);
 
     }
 
     // http://localhost:8089/tpfoyer/bloc/add-bloc
     @PostMapping("/add-bloc")
     public Bloc addBloc(@RequestBody Bloc c) {
-        Bloc bloc = blocService.addBloc(c);
-        return bloc;
+        return blocService.addBloc(c);
     }
 
     // http://localhost:8089/tpfoyer/bloc/remove-bloc/{bloc-id}
