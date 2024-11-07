6861c3eb2494ebf0aaee7c47105f588717efd0d0
azizturki111 <azizturki111@gmail.com>
2024-11-07 11:20:34 +0100

    Update ConfigAOP.java

--- src/main/java/tn/esprit/tpfoyer/config/ConfigAOP.java
+++ src/main/java/tn/esprit/tpfoyer/config/ConfigAOP.java
@@ -9,7 +9,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
-
+//
 @Component
 @Aspect
 @Slf4j
