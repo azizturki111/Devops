3322d3f82849e318883729b62ba783bc1f7b60f5
medazizturki <mohamedazizturkiii@gmail.com>
2024-11-07 09:31:07 +0100

    last push devops9

--- src/main/java/tn/esprit/tpfoyer/control/ReservationRestController.java
+++ src/main/java/tn/esprit/tpfoyer/control/ReservationRestController.java
@@ -47,11 +47,9 @@ public class ReservationRestController {
     // http://localhost:8089/tpfoyer/reservation/add-reservation
     @PostMapping("/add-reservation")
     public Reservation addReservation(@RequestBody Reservation r) {
-        Reservation reservation = reservationService.addReservation(r);
-        return reservation;
+        return reservationService.addReservation(r);
     }
 
-    // http://localhost:8089/tpfoyer/reservation/remove-reservation/{reservation-id}
     @DeleteMapping("/remove-reservation/{reservation-id}")
     public void removeReservation(@PathVariable("reservation-id") String rId) {
         reservationService.removeReservation(rId);
@@ -60,8 +58,7 @@ public class ReservationRestController {
     // http://localhost:8089/tpfoyer/reservation/modify-reservation
     @PutMapping("/modify-reservation")
     public Reservation modifyReservation(@RequestBody Reservation r) {
-        Reservation reservation = reservationService.modifyReservation(r);
-        return reservation;
+        return reservationService.modifyReservation(r);
     }
 
 }
